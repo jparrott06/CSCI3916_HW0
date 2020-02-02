5fd08b0bc15ffc68e29017098f6f28ae257ac6b3
jparrott06 <48459550+jparrott06@users.noreply.github.com>
2020-02-01 18:30:20 -0700

    foerhfoe

--- utils/googlebooks.js
+++ utils/googlebooks.js
@@ -8,19 +8,12 @@ module.exports = async (phrase) => {
         }
     })
 
-    return JSON.stringify(results.data);
+    var jsonData = {
+        data: results.data,
+        status: results.status,
+        statusText: results.statusText,
+        headers: results.headers,
+        requestHeader: results.config.headers
+    }
+    return JSON.stringify(jsonData);
 }
-
-/*  console.log(results.data);
-    console.log(results.status);
-    console.log(results.statusText);
-    console.log(results.headers);
-
-        const results = await axios({
-        method: 'get',
-        url: 'https://www.googleapis.com/books/v1/volumes',
-        params: {
-            format: 'json',
-            q: `${phrase}`,
-        },
-    })*/
